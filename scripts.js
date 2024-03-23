c575a61a9f6046ca91298e18f5a149d49c934252
Sasidharan S <sasidharan192000@gmail.com>
2024-03-24 00:36:16 +0530

    working product

--- scripts.js
+++ scripts.js
@@ -1,22 +1,6 @@
 //creating divs
 let container = document.querySelector(".container");
 
-/*let row = document.createElement("div");
-row.classList.add("row");
-container.appendChild(row);
-
-let column = document.createElement("div");
-column.classList.add("column");
-row.appendChild(column);
-
-let column2 = document.createElement("div");
-column2.classList.add("column");
-row.appendChild(column2);
-
-let column3 = document.createElement("div");
-column3.classList.add("column");
-row.appendChild(column3); */
-
 for(let rowCount=0; rowCount<16; rowCount++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -26,5 +10,27 @@ for(let rowCount=0; rowCount<16; rowCount++) {
         let column = document.createElement("div");
         column.classList.add("column");
         row.appendChild(column);
+    }
+}
+
+//generating the number of grid based on user input
+let button = document.querySelector(".userInput");
+button.addEventListener("click", buttonClickAction);
+
+function buttonClickAction() {
+    container.innerHTML = "";
+
+    let input = parseInt(prompt("Please enter the number if grids you need: "));
+
+    for(let rowCount=0; rowCount<input; rowCount++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+    
+        for(let columnCount=0; columnCount<input; columnCount++){
+            let column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
     }
 }
