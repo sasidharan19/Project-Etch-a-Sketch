8452514f191b8758af0db0c90fc4abad830c054b
Sasidharan S <sasidharan192000@gmail.com>
2024-03-24 16:58:20 +0530

    complete project

--- scripts.js
+++ scripts.js
@@ -9,6 +9,26 @@ for(let rowCount=0; rowCount<16; rowCount++) {
     for(let columnCount=0; columnCount<16; columnCount++){
         let column = document.createElement("div");
         column.classList.add("column");
+
+        function random_rgba() {
+            var o = Math.round, r = Math.random, s = 255;
+            return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+        }
+        let color = random_rgba();
+        
+        column.addEventListener("mouseover", () => {
+            column.style.backgroundColor = color;
+            column.style.opacity = "5";
+        });
+        
+        column.addEventListener("mouseout", () => {
+            column.style.backgroundColor = "darkgrey";
+        });
+
+        column.addEventListener("click", () => {
+            column.style.backgroundColor = "black";
+        })
+        
         row.appendChild(column);
     }
 }
@@ -22,15 +42,38 @@ function buttonClickAction() {
 
     let input = parseInt(prompt("Please enter the number if grids you need: "));
 
-    for(let rowCount=0; rowCount<input; rowCount++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        container.appendChild(row);
-    
-        for(let columnCount=0; columnCount<input; columnCount++){
-            let column = document.createElement("div");
-            column.classList.add("column");
-            row.appendChild(column);
+    if (input > 100) {
+        alert("Please enter the value less than 100");
+    } else {
+        for(let rowCount=0; rowCount<input; rowCount++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+        
+            for(let columnCount=0; columnCount<input; columnCount++){
+                let column = document.createElement("div");
+                column.classList.add("column");
+                function random_rgba() {
+                    var o = Math.round, r = Math.random, s = 255;
+                    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+                }
+                let color = random_rgba();
+                
+                column.addEventListener("mouseover", () => {
+                    column.style.backgroundColor = color;
+                    column.style.opacity = "5";
+                });
+                
+                column.addEventListener("mouseout", () => {
+                    column.style.backgroundColor = "darkgrey";
+                });
+        
+                column.addEventListener("click", () => {
+                    column.style.backgroundColor = "black";
+                })
+                
+                row.appendChild(column);
+            }
         }
     }
 }
